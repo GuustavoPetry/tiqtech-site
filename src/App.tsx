892439bcc2ac69628af1e49e9d5cd8d0f4c09eae
GuustavoPetry <ogustavopetry@gmail.com>
2026-02-27 11:41:13 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import BitrixWidget from "./components/BitrixWidget";
-import UTMHandler from "./components/UTMHandler"
+import UTMHandler from "./components/UTMHandler";
 
 const queryClient = new QueryClient();
 
